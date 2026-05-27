85310fad98cfcf52f9e5c49bee39c8a788f22e77
leonardoalmeida095 <leonardo.souza0995@gmail.com>
2026-05-26 21:43:51 -0300

    adicao bucket state

--- infra/main.tf
+++ infra/main.tf
@@ -1,5 +1,11 @@
 terraform {
   required_version = ">= 1.0.0"
+
+  backend "gcs" {
+    bucket  = "tfstate-projeto-a-492414"
+    prefix  = "terraform/state"
+  }
+
   required_providers {
     google = {
       source  = "hashicorp/google"
